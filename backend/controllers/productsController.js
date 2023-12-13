3249eda630bce6d4d2d7d68fe728946c2383f3bc
Santino <mellamoalroberto@gmail.com>
2023-12-12 23:54:13 -0300

    Change

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,12 +1,11 @@
 import Product from '../schemas/product.js'
-import newProduct from '../schemas/product.js'
 import fs from 'fs'
 
 const addProduct = async (req, res) => {
     try {
-        const product = new Product(req.body)
-        await product.save()
-        res.status(201).json(product)
+        const products = new Product(req.body)
+        await products.save()
+        res.status(201).json(products)
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
@@ -14,7 +13,7 @@ const addProduct = async (req, res) => {
 
 const getProducts = async (req, res) => {
     try {
-        const products = await newProduct.find();
+        const products = await Product.find();
         res.json(products);
     }
     catch (err) {
